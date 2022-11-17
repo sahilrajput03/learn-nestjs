4754cc259d2ccce4f23872017b8ce23715d0b472
Sahil Rajput <sahilrajput03@gmail.com>
2022-11-17 16:34:45 +0530

    update more

--- nestjs-sockets-TUTORIAL/socket-backend/src/chat.gateway.ts
+++ nestjs-sockets-TUTORIAL/socket-backend/src/chat.gateway.ts
@@ -2,6 +2,7 @@ import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer, Conne
 
 @WebSocketGateway()
 export class ChatGateway {
+  // Docs: You may want to have a direct access to the native, platform-specific server instance, so use the @WebSocketServer() decorator:
   @WebSocketServer()
   server;
 
@@ -10,15 +11,15 @@ export class ChatGateway {
     this.server.emit('message', message);
   }
 
-  // Passing `property key` to the decorator to extract it from the incoming message body:
+  // Docs: Passing `property key` to the decorator to extract it from the incoming message body:
   @SubscribeMessage('events')
   handleEvent(@MessageBody('id') id: number): number {
     // id === messageBody.id
     return id;
   }
 
-  // If you would prefer not to use decorators, the following code is functionally equivalent:
-  // This approach is not recommended though, because it requires mocking the socket instance in each unit test.
+  // Docs: If you would prefer not to use decorators, the following code is functionally equivalent:
+  // Docs: This approach is not recommended though, because it requires mocking the socket instance in each unit test.
   @SubscribeMessage('events')
   handleEvent(client: Socket, data: string): string {
     return data;
@@ -26,15 +27,14 @@ export class ChatGateway {
 
   @SubscribeMessage('events')
   handleEvent(@MessageBody() data: string,@ConnectedSocket() client: Socket): string {
-    // It's possible to emit messages using a library-specific approach, for example:
-    // client.emit()
+    // Docs: It's possible to emit messages using a library-specific approach, for example:
+    // Docs: client.emit()
 
     // Also:  In order to listen for messages emitted from within the above handler, the client has to attach a corresponding acknowledgment listener i.e.,
     // socket.emit('events', { name: 'Nest' }, (data) => console.log(data));
 
     return data;
   }
-  // However, in this case, you won't be able to leverage interceptors. If you don't want to respond to the user, you can simply skip the return statement (or explicitly return a "falsy" value, e.g. undefined).
+  // Docs: However, in this case, you won't be able to leverage interceptors. If you don't want to respond to the user, you can simply skip the return statement (or explicitly return a "falsy" value, e.g. undefined).
 
-  // continue from here: https://docs.nestjs.com/websockets/gateways#multiple-responses
 }
